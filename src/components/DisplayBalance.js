7d03aba4db8d9f054afe57b2a1c35d335ed8e8d3
Stewart Macias <macias.stewart@gmail.com>
2022-02-03 11:11:37 -0500

    Creating components

---
+++ src/components/DisplayBalance.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Statistic } from 'semantic-ui-react';
+
+function DisplayBalance( {title, value, size='tiny', color='black', textAlign='left'} ) {
+  return (
+    <Statistic size={size} color={color}>
+    <Statistic.Label style={{ textAlign: textAlign}}>
+      {title}
+    </Statistic.Label>
+    <Statistic.Value>
+        {value}
+    </Statistic.Value>
+  </Statistic>
+  );
+}
+
+export default DisplayBalance;
---
+++ src/components/DisplayBalances.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Grid, Segment } from 'semantic-ui-react';
+import DisplayBalance from './DisplayBalance';
+
+function DisplayBalances() {
+  return (
+    <Segment textAlign='center'>
+    <Grid columns={2} divided>
+      <Grid.Row>
+        <Grid.Column>
+
+          <DisplayBalance
+            color='green'
+            title='Income'
+            value='1,045.50'
+          />
+
+        </Grid.Column>
+        <Grid.Column>
+          <DisplayBalance
+            color='red'
+            title='Expenses'
+            value='623.50'
+          />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </Segment>
+  );
+}
+
+export default DisplayBalances;
