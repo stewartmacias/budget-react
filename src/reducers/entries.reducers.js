aed1c4ef6dfa94b6337d687823307b18ab5f41eb
Stewart Macias <macias.stewart@gmail.com>
2022-02-07 13:10:17 -0500

    implementing fork effect

--- src/reducers/entries.reducers.js
+++ src/reducers/entries.reducers.js
@@ -11,10 +11,13 @@ const reducer = (state = initialEntries, action) => {
         case entriesTypes.REMOVE_ENTRY:
             newEntries = state.filter(entry => entry.id !== action.payload.id);
             return newEntries;
+        case entriesTypes.POPULATE_ENTRY_DETAILS:
         case entriesTypes.UPDATE_ENTRY: 
+            console.log('a-> ', action);
             newEntries = [...state];
-            const index = newEntries.findIndex(entry => entry.id === action.payload.id);
-            newEntries[index] = {...action.payload.entry};
+            const index = newEntries.findIndex(
+                (entry) => entry.id === action.payload.id);
+            newEntries[index] = { ...newEntries[index],...action.payload.entry};
             return newEntries;
         default:
             return state;
